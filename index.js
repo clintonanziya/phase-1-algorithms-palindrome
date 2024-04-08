d3e18d00619c341668da0f041028a73131574846
clintonanziya <clintonanziya3@gmail.com>
2024-04-08 11:20:51 +0300

    completed-phase-1-algorithms-palindrome

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  Function isPalindrome:
+  1. Initialize two pointers, one starting from the beginning of the string and the other from the end.
+  2. While the start pointer is less than the end pointer:
+      a. If the characters at the two pointers are not equal, return false.
+      b. Move the start pointer forward and the end pointer backward.
+  3. If the loop completes without returning false, return true.
+
 }
 
 /* 
   Add your pseudocode here
 */
+Function isPalindrome:
+    Initialize two pointers, one starting from the beginning of the string and the other from the end.
+     While the start pointer is less than the end pointer:
+        a. If the characters at the two pointers are not equal, return false.
+        b. Move the start pointer forward and the end pointer backward.
+    If the loop completes without returning false, return true.
+
 
 /*
   Add written explanation of your solution here
-*/
+*/function isPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+  
+  while (start < end) {
+      if (str[start] !== str[end]) {
+          return false;
+      }
+      start++;
+      end--;
+  }
+  return true;
+}
+
+// Additional Test Cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("level")); // true
+console.log(isPalindrome("hello")); // false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
